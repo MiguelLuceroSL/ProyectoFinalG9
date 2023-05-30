b93fa91499833ad17b5982d9081f38fb440f8ff9
edumaldo94 <maldonado19994@gmail.com>
2023-05-30 15:29:37 -0300

    clases data,clases entidades, conexion

--- src/proyectofinalg9/ProyectoFinalG9.java
+++ src/proyectofinalg9/ProyectoFinalG9.java
@@ -1,7 +1,11 @@
 package proyectofinalg9;
+import AccesoADatos.Conexion;
 import java.util.Scanner;
 public class ProyectoFinalG9 {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in).useDelimiter("\n");
+     Conexion.getConexion();
+        
+        
     }
 }

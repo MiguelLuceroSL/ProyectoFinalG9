673dd4602e88da9bc848ad3068a3fd3a6519be6d
MiguelLuceroSL <theflixgame8@gmail.com>
2023-05-30 00:26:40 -0300

    Initial commit

---
+++ src/proyectofinalg9/ProyectoFinalG9.java
@@ -0,0 +1,7 @@
+package proyectofinalg9;
+import java.util.Scanner;
+public class ProyectoFinalG9 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+    }
+}
